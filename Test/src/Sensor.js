ad8632bfa3cd43112b69d132409c9f041edf6377
wininen <wini2woll@gmail.com>
2022-05-23 11:13:00 +0200

    attempting getting data from sensor

--- Test/src/Sensor.js
+++ Test/src/Sensor.js
@@ -1,43 +1,22 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import { BleManager } from 'react-native-ble-plx';
-import Base64 from 'base64-arraybuffer';
+//import Base64 from 'base64-arraybuffer';
+import {base64} from 'react-native-base64'
+import {Buffer} from 'buffer';
 
 const manager = new BleManager();
 manager.state = 'PoweredOn'
 console.log(manager.state);
 
 const scanAndConnect = async () => {
-  try {
-    const deviceId = "C4:7C:8D:6B:9A:41";
-    try { manager.cancelDeviceConnection(deviceId) } catch (e) {}
-    const connection = await manager.connectToDevice(deviceId, {
-      timeout: 1000
-    })
-    console.log("Connected!");
 
-    let device = await connection.discoverAllServicesAndCharacteristics();
-    for (const service of await device.services()) {
-      for (const c of await service.characteristics()) {
-        const char = device.readCharacteristicForService(c.serviceUUID, c.uuid);
-        const encodedValue = (await char).value;
-        if (encodedValue) {
-          // 1. Znaleźć lepszą bibliotekę do dekodowania
-          const value = Base64.decode(encodedValue);
-          console.log(Uint8Array.from(value));
-        }
+  
 
-        // console.log(characteristic.serviceID, characteristic.serviceUUID, characteristic.uuid, characteristic.id, characteristic.value)
-      }
-    }
-    connection.cancelConnection()
-  } catch(e) {
-    console.error(e)
-  }
 
-  return;
 
     manager.startDeviceScan(null, null, async (error, device) => {
+      
         if (error) {
             // Handle error (scanning will be stopped automatically)
             console.log("nie widzę urządzenia")
@@ -52,59 +31,86 @@ const scanAndConnect = async () => {
         console.log(device.name)
         if (device.name === 'Flower care') {
             console.log("JEST")
+            
 
+
+            try{
+            const connection = await device.connect()
+            console.log("Connected!");
+
+            let dev = await connection.discoverAllServicesAndCharacteristics();
+            //console.log(dev)
+            let result = dev.readCharacteristicForService("00001204-0000-1000-8000-00805f9b34fb", "00001a01-0000-1000-8000-00805f9b34fb");
+            console.log(result)
+
+
+            const sub = await manager.monitorCharacteristicForDevice(device.id, "00001204-0000-1000-8000-00805f9b34fb", 
+            "00001a01-0000-1000-8000-00805f9b34fb",
+            (error, result) => {
+              console.log("MONITORED =", result);
+              if (error) {
+                console.log("ERROR ON MONITOR =", error);
+                return;
+              }
+              console.log("MONITORED =", result.value);
+            });
+
+            //let result = dev.readCharacteristicForService("00001204-0000-1000-8000-00805f9b34fb", "00001a01-0000-1000-8000-00805f9b34fb");
+            //const r =  (await result).value;
+            //const b = Buffer.from(r, 'base64');
+            //console.log(b);
+            //console.log("temperature: " + b.readUint16LE() * 0.01);
+
+
+            connection.cancelConnection()
+          }
+          catch(e){
+            console.log("error");
+            console.error(e)
+          }
+            /*
             device.connect()
             .then((device) => {
-              device.characteristicsForService("38")
-                .catch((...args) => console.log("error!!!!: ", args))
-                .then((...args) => console.log("xd", args));
-                console.log(device.discoverAllServicesAndCharacteristics())
+              //device.characteristicsForService("38")
+                //.catch((...args) => console.log("error!!!!: ", args))
+                //.then((...args) => console.log("xd", args));
+                //console.log(device.discoverAllServicesAndCharacteristics())
+                console.log("connected!")
                 return device.discoverAllServicesAndCharacteristics()
             })
             .then((device) => {
             // Do work on device with services and characteristics
-
-            //const services = device.services();
-            //console.log("Services:",services);
-            //const characteristics = services[1].characteristics;
-            //console.log("Characteristics:",characteristics);
-  
-
-              //const characteristic = None
-            //for (let m=0; m <5; m++){
-
-            console.log("Przed services")
-
-              device.services().then(services => {
-                services.forEach((service, i) => {
-                  service.characteristics().then(c => {
-                    console.log('----' + i + '-----');
-                    console.log(c); 
-                    
-                    //console.log("Is Characteristics Readable:",c[0].isReadable);
-                   // console.log(c[0].value)
-
-                    /*
-                    c[0].monitor((err, update) => {
-                      if (err) {
-                        console.log(`characteristic error: ${err}`);
-                        console.log(JSON.stringify(err));
-                      } else {
-                        console.log("Is Characteristics Readable:",update.isReadable);
-                        console.log(update.value)
-                      }
-                    });
-                   */
-
-
-                    //console.log(device.readCharacteristicForService(service.uuid, characteristic.uuid))
-                    return c
-                  });
-                });
-              });
+            console.log("working on service...")
+            const n = device.readCharacteristicForService("00001204-0000-1000-8000-00805f9b34fb", "00001a01-0000-1000-8000-00805f9b34fb");
+            console.log(n)
+            const encodedN = n.value;
+            console.log(encodedN);
+            
+            if (encodedN) {
+              console.log("encodedN: ")
+               console.log(encodedN)
+        
+              //console.log("base64: ");
+              //console.log(base64.decode('SW4gbXkgZXllcywgaW5kaXNwb3NlZA0KSW4gZGlzZ3Vpc2VzIG5vIG9uZSBrbm93cw0KUklQIEND=='))
+        
+               const buff = Buffer.from(encodedN, 'base64');
+               console.log(buff);
+               console.log("temperature: " + buff.readUint16LE() * 0.01);
+               const brightness = buff.readUInt8(3);
               
-            //}
-
+               console.log("brightness: "+ brightness)
+               const moisture = buff.readUInt16LE(6);
+               console.log("moisture: "+ moisture)
+               //console.log("buff utf8");
+               //console.log(buff.toString('utf-8'));
+               //console.log("buff string");
+               //console.log(buff.toString('ascii'));
+               //console.log("the end");
+                // console.log(characteristic.serviceID, characteristic.serviceUUID, characteristic.uuid, characteristic.id, characteristic.value)
+              }
+              else {
+                console.log("nie widzę nic")
+              }
               
 
             })
@@ -113,6 +119,7 @@ const scanAndConnect = async () => {
                 console.log(error.message);
             });
 
+              */
             
 
             //console.log(manager.readCharacteristicForDevice(device.id))
