d418fa6a3e4281098a3ecfac7837e3d767e60342
wininen <wini2woll@gmail.com>
2022-05-27 21:45:14 +0200

    real-time data from sensor

--- Test/src/Sensor.js
+++ Test/src/Sensor.js
@@ -1,160 +1,192 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import { BleManager } from 'react-native-ble-plx';
-//import Base64 from 'base64-arraybuffer';
-import {base64} from 'react-native-base64'
+import React from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import BleManager from "react-native-ble-manager";
+import {useState} from 'react';
+import {PermissionsAndroid, NativeModules, NativeEventEmitter } from 'react-native';
 import {Buffer} from 'buffer';
 
-const manager = new BleManager();
-manager.state = 'PoweredOn'
-console.log(manager.state);
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+//let peripheralFlower = undefined
 
-const scanAndConnect = async () => {
+
+
+
+const Sensor = () => {
+
+  const [flower_care, setFlowerCare] = useState([]);
+  const peripherals = new Map();
+
+
+
+  //funkcja obsługująca wyszukiwanie urządzeń
+  const handleDiscoverPeripheral = (peripheral) => {
+    if(peripheral.name == "Flower care"){
+
+      if (peripherals.size == 0){
+        peripherals.set(peripheral.id, peripheral);
+        setFlowerCare(peripheral);
+      }
+      
+    }
+  }
+
+
+  const handleStopScan  = () => {
+    console.log("done")
+  }
+
+
+  //funkcja obsługująca odłączenie urządzenia, jeszcze nie skonfigurowany
+  const handleDisconnectedPeripheral = (data) => {
+    console.log("handleDisconnectedPeripheral")
+    let peripheral = peripherals.get(data.peripheral);
+    if (peripheral) {
+      peripheral.connected = false;
+      peripherals.set(peripheral.id, peripheral);
+      setFlowerCare(Array.from(peripherals.values()));
+    }
+    console.log('Disconnected from ' + data.peripheral);
+  }
 
   
+  //funkcja obsługująca rejestrowanie nowych wartości konkretnej "characteristics"
+  const handleUpdateValueForCharacteristic = (data) => {
+
+    
+    const inputData = Buffer.from(data.value);
+
+    //odkodowuje bity
+    temperature = inputData.readUint16LE(0) / 10
+    light = inputData.readIntLE(3,4)
+    //light = inputData.readUInt32LE(3)
+    moist = inputData.readUInt8(7)
+    fertility = inputData.readUint16LE(8)
+
+    console.log("\n")
+    
+    console.log(`Recieved for characteristic! ${data.characteristic}  temperature: ${temperature}  light: ${light}   moist: ${moist}  fertility: ${fertility}`);
+    console.log("\n");
+  }
 
 
 
-    manager.startDeviceScan(null, null, async (error, device) => {
+  //bleManagerEmmiter obsługuje eventy
+  bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', handleDiscoverPeripheral)
+
+  bleManagerEmitter.addListener( 'BleManagerStopScan',handleStopScan)
+
+  bleManagerEmitter.addListener('BleManagerDidUpdateValueForCharacteristic', handleUpdateValueForCharacteristic );
+
+  //bleManagerEmitter.addListener('BleManagerDisconnectPeripheral', handleDisconnectedPeripheral );
+
+
+
+  //funkcja skanująca urządzenia
+  const scan = async () => { 
+
+  
+    console.log("started")
+    await BleManager.start( { forceLegacy: true } )
+
+    //console.log("check location access permission")
+    //await getBluetoothScanPermission()
+
+    await BleManager.scan([], 2).then(() => {
+      console.log('Scanning...');
+    })
+
+
       
-        if (error) {
-            // Handle error (scanning will be stopped automatically)
-            console.log("nie widzę urządzenia")
-            console.log(error)
-            return
-        }
+  
+  }
+
+
+  //funkcja łącząca się z sensorem
+  const connect = async (peripheral, service ="00001204-0000-1000-8000-00805f9b34fb", characteristic = "00001a01-0000-1000-8000-00805f9b34fb") => { 
+
+    const charwrite = "00001a00-0000-1000-8000-00805f9b34fb"
+
+    console.log("trying to connect:")
+    console.log(peripheral.id)
+
+
+      //połącz się z czujnikiem      
+      await BleManager.connect(peripheral.id).then(() => {
+        console.log('Connected to ' + peripheral.id);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+      
+
+      //odnajduje services i characteristics danego urządzenia
+      //trzeba zawsze uruchomić najpierw przed uruchomieniem metod write, read i start notification 
+      await BleManager.retrieveServices(peripheral.id).then((peripheralData) => {
+        console.log('Retrieved peripheral services'); 
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+      
+      //Wpsijemu wartość [0xa0, 0x1f] do characteristics "charwrite" żeby uruchomić odczytywanie w czasie rzeczywistym
+      await BleManager.write(peripheral.id, service, charwrite, [0xa0, 0x1f])
+      .then(() => {
+          console.log("Enabled real-time data!");
+      })
+      .catch((error) => {
+          console.log(error);
+      });
+
+
+      //Start the notification on the specified characteristic, you need to call retrieveServices method before. 
+      //The buffer will collect a number or messages from the server and then emit once the buffer count it reached. 
+      //Helpful to reducing the number or js bridge crossings when a characteristic is sending a lot of messages. Returns a Promise objec
+      
+      await BleManager.startNotification(peripheral.id, service, characteristic).then(() => {
+          console.log('started notifications of:  ' + peripheral.id);
+      })
+      .catch((error) => {
+          console.log(error);
+      });
         
-
-        // Check if it is a device you are looking for based on advertisement data
-        // or other criteria.
-        console.log("scanning....")
-        console.log(device.name)
-        if (device.name === 'Flower care') {
-            console.log("JEST")
-            
-
-
-            try{
-            const connection = await device.connect()
-            console.log("Connected!");
-
-            let dev = await connection.discoverAllServicesAndCharacteristics();
-            //console.log(dev)
-            let result = dev.readCharacteristicForService("00001204-0000-1000-8000-00805f9b34fb", "00001a01-0000-1000-8000-00805f9b34fb");
-            console.log(result)
-
-
-            const sub = await manager.monitorCharacteristicForDevice(device.id, "00001204-0000-1000-8000-00805f9b34fb", 
-            "00001a01-0000-1000-8000-00805f9b34fb",
-            (error, result) => {
-              console.log("MONITORED =", result);
-              if (error) {
-                console.log("ERROR ON MONITOR =", error);
-                return;
-              }
-              console.log("MONITORED =", result.value);
-            });
-
-            //let result = dev.readCharacteristicForService("00001204-0000-1000-8000-00805f9b34fb", "00001a01-0000-1000-8000-00805f9b34fb");
-            //const r =  (await result).value;
-            //const b = Buffer.from(r, 'base64');
-            //console.log(b);
-            //console.log("temperature: " + b.readUint16LE() * 0.01);
-
-
-            connection.cancelConnection()
-          }
-          catch(e){
-            console.log("error");
-            console.error(e)
-          }
-            /*
-            device.connect()
-            .then((device) => {
-              //device.characteristicsForService("38")
-                //.catch((...args) => console.log("error!!!!: ", args))
-                //.then((...args) => console.log("xd", args));
-                //console.log(device.discoverAllServicesAndCharacteristics())
-                console.log("connected!")
-                return device.discoverAllServicesAndCharacteristics()
-            })
-            .then((device) => {
-            // Do work on device with services and characteristics
-            console.log("working on service...")
-            const n = device.readCharacteristicForService("00001204-0000-1000-8000-00805f9b34fb", "00001a01-0000-1000-8000-00805f9b34fb");
-            console.log(n)
-            const encodedN = n.value;
-            console.log(encodedN);
-            
-            if (encodedN) {
-              console.log("encodedN: ")
-               console.log(encodedN)
-        
-              //console.log("base64: ");
-              //console.log(base64.decode('SW4gbXkgZXllcywgaW5kaXNwb3NlZA0KSW4gZGlzZ3Vpc2VzIG5vIG9uZSBrbm93cw0KUklQIEND=='))
-        
-               const buff = Buffer.from(encodedN, 'base64');
-               console.log(buff);
-               console.log("temperature: " + buff.readUint16LE() * 0.01);
-               const brightness = buff.readUInt8(3);
-              
-               console.log("brightness: "+ brightness)
-               const moisture = buff.readUInt16LE(6);
-               console.log("moisture: "+ moisture)
-               //console.log("buff utf8");
-               //console.log(buff.toString('utf-8'));
-               //console.log("buff string");
-               //console.log(buff.toString('ascii'));
-               //console.log("the end");
-                // console.log(characteristic.serviceID, characteristic.serviceUUID, characteristic.uuid, characteristic.id, characteristic.value)
-              }
-              else {
-                console.log("nie widzę nic")
-              }
-              
-
-            })
-            .catch((error) => {
-                // Handle errors
-                console.log(error.message);
-            });
-
-              */
-            
-
-            //console.log(manager.readCharacteristicForDevice(device.id))
-            // Stop scanning as it's not necessary if you are scanning for one device.
-            
-            manager.stopDeviceScan();
-            return
-            // Proceed with connection.
-        }
-    });
 
 }
 
-//console.log("here")
 
 
+  
+  return (
+    <View style={styles.container}>
+      <Button
+        onPress={scan}
+      title="Wyszukaj urządzenie"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <Text></Text>
 
-
-export default class Sensor extends Component
-{
-  render()
-  {
-    return (
-      <View style={styles.container}>
-        <Text onPress={scanAndConnect}> Hello World  tutaj!</Text>
-      </View>
-    );
-  }
+      <Button style = {{ width : 150, height : 150, marginLeft : 370 }}
+              onPress={() => connect(flower_care)}
+              //onPress={connectAndPrepare}
+              title="połącz"
+              color="#841584"
+              accessibilityLabel="Learn more about this purple button"
+        />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#fff',
-        paddingTop: Platform.OS === "android" ? 25 : 0,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-  });
+  container: {
+      backgroundColor: '#fff',
+      paddingTop: Platform.OS === "android" ? 300 : 0,
+      paddingBottom:300,
+      alignItems: 'center',
+      justifyContent: 'center'
+  },
+
+});
+
+export default Sensor;
