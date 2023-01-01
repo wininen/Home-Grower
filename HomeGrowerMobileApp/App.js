fc7509671a76c72800cca2ba15cc9efada42a6f0
AgataWojciech <agata-wojciechowska99@wp.pl>
2023-01-01 15:57:52 +0100

    SQLite verry basic connection

--- HomeGrowerMobileApp/App.js
+++ HomeGrowerMobileApp/App.js
@@ -8,11 +8,29 @@ import Sensor from './src/Sensor.js';
 import Plants from './src/Plants.js';
 import Forecast from './src/Forecast.js';
 import Profile from './src/Profile.js';
+// for SQLite database 
+import SQLite from 'react-native-sqlite-storage';
+let SQLiteDB;
+
 
 const Main = createNativeStackNavigator();
 
 
 const App = () => {
+    SQLite.openDatabase({
+        name: 'plantsSQLite.db',
+        createFromLocation: 1,
+      },
+      this.successToOpenDB,
+      this.failToOpenDB,
+    );
+
+    const successToOpenDB = async() => {
+        alert('success');
+    }
+    const failToOpenDB = async(err) => {
+        console.log(err);
+    }
 
     return (
       <NavigationContainer>
