f1578fb84b00f2ea22599e5cbffd62378293f254
gabrysiuu20 <gabrylosow1999@gmail.com>
2023-01-07 21:14:33 +0100

    configured jest and some fixes

--- HomeGrowerMobileApp/src/components/Nav/Nav.styled.js
+++ HomeGrowerMobileApp/src/components/Nav/Nav.styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 import Entypo from 'react-native-vector-icons/Entypo';
 import {h6} from '../../styles/typography.styled';
 
